961aea1170301afbc45fc5cd567fe40ebb01a364
Oliver Wegner <void1976@gmail.com>
2017-03-17 12:36:22 +0100

    implementing select-lazy

--- src/clj/eventsourcing/db.clj
+++ src/clj/eventsourcing/db.clj
@@ -3,7 +3,8 @@
             [korma.core :refer :all]
             [heroku-database-url-to-jdbc.core :as h]
             [environ.core :as env]
-            [clojure.core.logic :refer :all]))
+            [clojure.core.logic :refer :all]
+            [clojure.core.logic.pldb :refer :all]))
 
 (defn open []
   (let [url (env/env :database-url)
@@ -11,3 +12,9 @@
         db (db/create-db (db/postgres kmap))]
     (db/default-connection db)
     db))
+
+(defn select-lazy
+  ([q l o] (when-let [res (seq (select (-> (q) (limit l) (offset o))))]
+             (println "did another select from" o "to" (+ o l))
+             (cons res (lazy-seq (select-lazy q l (+ o l))))))
+  ([q] (select-lazy q 1 0)))
