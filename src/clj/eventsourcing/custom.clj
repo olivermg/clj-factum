839fd93ecc92844b1aa40999240b869ea31e60e7
Oliver Wegner <void1976@gmail.com>
2017-03-24 09:59:13 +0100

    implement custom conj-entity reduce fn that will only pick first occurrence of an entity's attribute

--- src/clj/eventsourcing/custom.clj
+++ src/clj/eventsourcing/custom.clj
@@ -1,9 +1,12 @@
 (ns eventsourcing.custom
-  (:require [clojure.core.logic :as l]))
+  (:require [clojure.edn :as edn]
+            [clojure.core.logic :as l]
+            [korma.core :as db]
+            [eventsourcing.db :as evdb]))
 
 (defrecord Fact [e a v t])
 
-(def db [(->Fact :user :name "foo" 1)
+#_(def db [(->Fact :user :name "foo" 1)
          (->Fact :user :age 23 2)
          (->Fact :user :name "bar" 3)
          (->Fact :user :age 25 4)])
@@ -11,26 +14,58 @@
 (extend-type Fact
   clojure.core.logic.protocols/IUnifyTerms
   (unify-terms [u v s]
-    (println "U:" u ", V:" v ", S:" s)
+    ;;;(println "U:" u ", V:" v ", S:" s)
     (when (and (instance? clojure.lang.PersistentVector v)
                (> (count v) 1))
       (loop [i 0 v v s s]
-        (println "I:" i ", V:" v ", S:" s)
+        ;;;(println "I:" i ", V:" v ", S:" s)
         (if (empty? v)
           s
           (when-let [s (l/unify s (first v) (get u (nth [:e :a :v :t] i)))]
             (recur (inc i) (next v) s)))))))
+
+(defn get-events []
+  (let [query (-> (db/select* evdb/es_events)
+                  (db/order :tx :desc))
+        xf (comp (map #(->Fact (:eid %)
+                               (-> % :attribute edn/read-string)
+                               (-> % :value edn/read-string)
+                               (:tx %)))
+                 #_(fn [xf]  ;; does not work, as volatile state only holds during single transduce invocation
+                     (let [entities (volatile! {})]
+                       (fn
+                         ([] (xf))
+                         ([result] (xf result))
+                         ([result {:keys [e a] :as input}]
+                          (println "3:" e a @entities)
+                          (if-not (get-in @entities [e a])
+                            (do (vswap! entities #(assoc-in % [e a] true))
+                                (xf result input))
+                            result))))))
+        entities (volatile! {})
+        conj-entity (fn
+                      ([] [])
+                      ([s] s)
+                      ([s {:keys [e a] :as entity}]
+                       (if-not (get-in @entities [e a])
+                         (do (vswap! entities #(assoc-in % [e a] true))
+                             (conj s entity))
+                         s)))]
+    (evdb/select-lazy query xf conj-entity)))
 
 (defn fact-rel [q]
   (fn [a]
     (l/to-stream
      (map #(l/unify a % q)
           #_(sort-by :t > db)
-          (eventsourcing.db/get-events)
+          (get-events)
           ))))
 
-#_(l/run 5 [q]
+#_(evdb/open)
+#_(get-events)
+#_(l/run 2 [q]
     (l/fresh [e a v t]
       (l/== a :user/name)
+      ;;;(l/== e 1)
       (fact-rel [e a v t])
       (l/== q [e a v t])))
--- src/clj/eventsourcing/db.clj
+++ src/clj/eventsourcing/db.clj
@@ -5,10 +5,10 @@
             [korma.core :refer :all]
             [heroku-database-url-to-jdbc.core :as h]
             [environ.core :as env]
-            [clojure.core.logic :refer :all]
-            [clojure.core.logic.pldb :refer :all]
-            [clojure.edn :as edn]
-            [eventsourcing.custom :as es]))
+            #_[clojure.core.logic :refer :all]
+            #_[clojure.core.logic.pldb :refer :all]
+            #_[clojure.edn :as edn]
+            #_[eventsourcing.custom :as es]))
 
 (defentity es_events)
 
@@ -21,30 +21,22 @@
 
 (defn select-lazy
   "q is a korma select object, produced via select*"
-  ([q l o xf] (when-let [res (-> (select (-> q (limit l) (offset o)))
-                                 (#(if xf
-                                     (transduce xf conj %)
-                                     %))
-                                 seq)]
-                (println "did another select from" o "to" (+ o l))
-                (concat res (lazy-seq (select-lazy q l (+ o l) xf)))))
-  ([q xf] (select-lazy q 1 0 xf))
-  ([q] (select-lazy q 1 0 nil)))
+  ([q l o xf rf] (when-let [res (-> (select (-> q (limit l) (offset o)))
+                                    (#(if xf
+                                        #_(into [] xf %)
+                                        (transduce xf (or rf conj) %)
+                                        %))
+                                    seq)]
+                   (println "did another select from" o "to" (+ o l))
+                   (concat res (lazy-seq (select-lazy q l (+ o l) xf rf)))))
+  ([q xf rf] (select-lazy q 2 0 xf rf))
+  ([q xf] (select-lazy q 2 0 xf nil))
+  ([q] (select-lazy q 2 0 nil nil)))
 
 
 (defn entity [eid]
   (select es_events
           (where {:eid eid})))
 
-(defn get-events []
-  (select-lazy (-> (select* es_events)
-                   (order :tx :desc))
-               (map #(es/->Fact (:eid %)
-                                (-> % :attribute edn/read-string)
-                                (-> % :value edn/read-string)
-                                (:tx %)))))
-
-
 #_(open)
-#_(get-events)
 #_(entity 1)
