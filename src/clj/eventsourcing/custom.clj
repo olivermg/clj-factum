621eeacbaecc9a9387855f14bf3558ba31be5379
Oliver Wegner <void1976@gmail.com>
2017-03-24 15:32:17 +0100

    implementing retracting facts

--- src/clj/eventsourcing/custom.clj
+++ src/clj/eventsourcing/custom.clj
@@ -6,7 +6,11 @@
 
 (defrecord Fact [e a v t])
 
-(db/defentity es_events)
+(db/defentity es_events
+  (db/prepare (fn [v] (reduce #(update %1 %2 pr-str)
+                              v #{:action :attribute :value})))
+  (db/transform (fn [v] (reduce #(update %1 %2 edn/read-string)
+                                v #{:action :attribute :value}))))
 
 #_(def db [(->Fact :user :name "foo" 1)
          (->Fact :user :age 23 2)
@@ -21,19 +25,24 @@
                       %))
                   (db/order :tx :desc))
         facts (volatile! {}) ;; need to define it outside xf, as select-lazy transduces multiple times
-        xf (comp (map #(->Fact (:eid %)
-                               (-> % :attribute edn/read-string)
-                               (-> % :value edn/read-string)
-                               (:tx %)))
-                 (fn [xf]
+        xf (comp (fn [xf]
                    (fn
                      ([] (xf))
                      ([result] (xf result))
-                     ([result {:keys [e a] :as input}]
-                      (if-not (get-in @facts [e a])
-                        (do (vswap! facts #(assoc-in % [e a] true))
-                            (xf result input))
-                        result)))))]
+                     ([result {:keys [action eid attribute] :as input}]
+                      (case action
+                        :add (case (get-in @facts [eid attribute])
+                               true result
+                               ::retracted (do (vswap! facts #(update-in % [eid] dissoc attribute))
+                                               result)
+                               nil (do (vswap! facts #(assoc-in % [eid attribute] true))
+                                       (xf result input)))
+                        :retract (do (vswap! facts #(assoc-in % [eid attribute] ::retracted))
+                                     result)))))
+                 (map #(->Fact (:eid %)
+                               (:attribute %)
+                               (:value %)
+                               (:tx %))))]
     (evdb/select-lazy query xf)))
 
 (defn get-entity [eid]
@@ -45,29 +54,40 @@
             {}
             facts)))
 
-(defn- get-eid []
+(defn- new-eid []
   #_(str (java.util.UUID/randomUUID))
   (long (rand java.lang.Long/MAX_VALUE)))
 
-(defn- get-txid []
+(defn- new-txid []
   (-> (db/select (db/sqlfn nextval "es_events_txid"))
       first
       :nextval))
 
-(defn add-fact [{:keys [e a v t] :as fact}]
+(defn- save-fact [{:keys [e a v t] :as fact} & {:keys [action]
+                                                :or {action :add}}]
   (let [data (db/insert es_events
-                        (db/values {:eid (or e (get-eid))
-                                    :attribute (pr-str a)
-                                    :value (pr-str v)
-                                    :tx (or t (get-txid))}))]
-    (->Fact (:eid data) (-> data :attribute edn/read-string)
-            (-> data :value edn/read-string) (:tx data))))
+                        (db/values {:eid (or e (new-eid))
+                                    :attribute a
+                                    :value v
+                                    :tx (or t (new-txid))
+                                    :action action}))]
+    (->Fact (:eid data) (:attribute data)
+            (:value data) (:tx data))))
 
 (defn add-facts [facts]
   (let [txid (get-txid)]
-    (->> (map #(assoc % :t txid) facts)
-         (map add-fact)
-         doall)))
+    #_(->> (map #(assoc % :t txid) facts)
+           (map save-fact)
+         doall)
+    (sequence (comp (map #(assoc % :t txid))
+                    (map #(save-fact % :action :add)))
+              facts)))
+
+(defn retract-facts [facts]
+  (let [txid (get-txid)]
+    (sequence (comp (map #(assoc % :t txid))
+                    (map #(save-fact % :action :retract)))
+              facts)))
 
 
 (extend-type Fact
@@ -94,7 +114,7 @@
 
 #_(evdb/open)
 #_(get-events)
-#_(l/run 2 [q]
+#_(l/run* [q]
     (l/fresh [e a v t]
       (l/== a :user/name)
       ;;;(l/== e 1)
