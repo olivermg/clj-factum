4f28b19326f940269c6e6370662e57a77f4b42f2
Oliver Wegner <void1976@gmail.com>
2017-04-28 14:00:39 +0200

    remove obsolete dependency

--- transports/embedded/project.clj
+++ transports/embedded/project.clj
@@ -4,9 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.442"]
-
-                 [clj-factum/serverlib "0.1.1-SNAPSHOT"]]
+                 [org.clojure/core.async "0.3.442"]]
 
   :min-lein-version "2.6.1"
 
