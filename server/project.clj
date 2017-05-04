5727230d7f00f77909b2bb4a96c661e661c4bc05
Oliver Wegner <void1976@gmail.com>
2017-05-04 15:44:40 +0200

    use clj-chatterbox

--- server/project.clj
+++ server/project.clj
@@ -5,13 +5,24 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.442"]
+                 [com.stuartsierra/component "0.3.2"]
+                 [com.taoensso/sente "1.11.0"]
 
                  [clj-factum/serverlib "0.1.1-SNAPSHOT"]
 
                  ;;;[environ "1.1.0"]
 
-                 ;;; tcp server:
-                 [aleph "0.4.3"]
+                 ;;;[liberator "0.14.1"]
+                 ;;;[bidi "2.0.17"]
+                 ;;;[hiccup "1.0.5"]
+                 ;;;[cheshire "5.7.1"]
+                 ;;;[metosin/compojure-api "1.1.10"]
+                 ;;;[buddy/buddy-auth "1.4.1"]
+
+                 ;;; tcp/http server:
+                 ;;;[aleph "0.4.3"]
+                 [clj-chatterbox "0.1.0-SNAPSHOT"]
                  ]
 
   ;;;:plugins [[lein-environ "1.1.0"]]
@@ -24,7 +35,7 @@
 
   :main ^:skip-aot ow.factum.server
 
-  ;;;:target-path "target/%s"
+  :target-path "target/%s"
 
   :profiles {:uberjar {:aot :all}}
 
--- server/src/clj/ow/factum/server.clj
+++ server/src/clj/ow/factum/server.clj
@@ -1,16 +1,68 @@
 (ns ow.factum.server
   (:require [clojure.edn :as edn]
-            [manifold.stream :as s]
-            [manifold.deferred :as d]
-            [aleph.tcp :as tcp]
-            [aleph.netty :as netty])
+            [clojure.core.async :refer [go-loop <!]]
+            [com.stuartsierra.component :as c]
+            [taoensso.sente :as sente]
+            #_[manifold.stream :as s]
+            #_[manifold.deferred :as d]
+            #_[aleph.tcp :as tcp]
+            #_[aleph.netty :as netty]
+            #_[bidi.bidi :refer [match-route path-for] :as bb]
+            #_[liberator.core :refer [defresource resource] :as l]
+            #_[buddy.sign.jwt :as jwt]
+            #_[buddy.auth :refer [authenticated? throw-unauthorized]]
+            [ow.chatterbox.core :refer [webapp]])
   (:import [java.io FileNotFoundException])
   (:gen-class))
 
-(def ^:private +default-config+
-  {:port 10001})
+#_(defresource login
+  :allowed-methods [:get]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [{:keys [request] :as ctx}]
+               {:foo "bar"}))
 
-(defn- echo-server [s info]
+#_(def api-routes ["/api/login" login])
+
+(def ^:private +default-config+
+  {:webserver (let [jsecret-api "foofoo"
+                    jsecret-ws "barbar"
+
+                    #_api-handler #_(fn [req]
+                                  (println "api")
+                                  #_(case (:uri req)
+                                    "/api/login" {:status 200
+                                                  :body (str "{ \"token\": \""
+                                                             (jwt/sign {:user 123} jsecret-api)
+                                                             "\" }")
+                                                  :headers {"Content-Type" "application/json"}}
+                                    "/api/authed" (do (when (not (authenticated? req))
+                                                        (throw-unauthorized {:message "not authorized for api"}))
+                                                      {:status 200
+                                                       :body "private api content"
+                                                       :headers {"Content-Type" "application/json"}})
+                                    {:status 200
+                                     :body "public api content"
+                                     :headers {"Content-Type" "application/json"}})
+                                  (when-let [{:keys [handler]} (match-route api-routes (:uri req))]
+                                    (handler req)))
+
+                    ws-handler-setup (fn [ch-recv send-fn connected-uids]
+                                       (println "ws")
+                                       (go-loop [{:keys [event ?reply-fn] :as msg} (<! ch-recv)]
+                                         (println "got ws event:" event)
+                                         (when ?reply-fn
+                                           (?reply-fn {:foo "bar"}))
+                                         (recur (<! ch-recv))))]
+
+                {:server {:port 8899}
+                 :all {:dev? true}
+                 #_:api #_{:handler api-handler
+                       :prefix "/api/"
+                       :jws-secret jsecret-api}
+                 :ws {:handler-setup ws-handler-setup
+                      :prefix "/ws"}})})
+
+#_(defn- echo-server [s info]
   (println s info)
   (s/connect s s))
 
@@ -24,8 +76,9 @@
 
 (defn -main [& args]
   (print "starting... ")
-  (let [{:keys [port] :as cfg} (config)
-        s (tcp/start-server echo-server {:port port})]
+  (let [cfg (config)
+        webapp (webapp cfg) #_(tcp/start-server echo-server {:port port})]
     (println "done.")
-    (netty/wait-for-close s)
+    (c/start webapp)
+    ;;;(netty/wait-for-close s)
     (println "quit.")))
