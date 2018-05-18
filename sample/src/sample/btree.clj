920b7359e8a9fcb0f1149647fac907f4c62d8944
Oliver Wegner <void1976@gmail.com>
2018-05-18 12:56:41 +0200

    RangeMap:
    - add leftfn & rightfn depending on kind
    - don't count on every loop iteration

--- sample/src/sample/btree.clj
+++ sample/src/sample/btree.clj
@@ -8,16 +8,15 @@
   (search [this k]))
 
 
-(deftype RangeMap [kind ^clojure.lang.IPersistentMap m]
+(deftype RangeMap [kind ^clojure.lang.IPersistentMap m leftfn rightfn]
 
   clojure.lang.IPersistentMap
 
   (assoc [this k v]
-    (->RangeMap kind (.assoc m k v)))
+    (->RangeMap kind (.assoc m k v) leftfn rightfn))
 
   (assocEx [this k v]
-    (.assocEx m k v)
-    this)
+    (->RangeMap kind (.assocEx m k v) leftfn rightfn))
 
 
   clojure.lang.ILookup
@@ -27,17 +26,17 @@
 
   (valAt [this k not-found]
     (let [idx (loop [ks     (keys m)
+                     n      (count ks)
                      lastki nil]
                 #_(println "LOOP1" lastki ks)
                 (if (not-empty ks)
-                  (let [n   (count ks)
-                        i   (int (/ n 2))
+                  (let [i   (int (/ n 2))
                         ki  (get (vec ks) i)
                         cmp (compare ki k)]
                     #_(println "LOOP2" n i ki cmp)
                     (cond
-                      (> cmp 0) (recur (take i ks)       (if (= kind :find-floor)   lastki ki))  ;; traverse left
-                      (< cmp 0) (recur (drop (inc i) ks) (if (= kind :find-ceiling) lastki ki))  ;; traverse right
+                      (> cmp 0) (recur (take i ks)       i         (leftfn lastki ki))   ;; traverse left
+                      (< cmp 0) (recur (drop (inc i) ks) (- n i 1) (rightfn lastki ki))  ;; traverse right
                       (= cmp 0) ki))
                   lastki))]
       (get m idx))))
@@ -45,7 +44,12 @@
 (defn range-map [kind & kvs]
   {:pre [(or (= kind :find-ceiling)
              (= kind :find-floor))]}
-  (->RangeMap kind (apply sorted-map kvs)))
+  (let [[leftfn rightfn] (case kind
+                           :find-floor   [(fn [lastki ki] lastki)
+                                          (fn [lastki ki] ki)]
+                           :find-ceiling [(fn [lastki ki] ki)
+                                          (fn [lastki ki] lastki)])]
+    (->RangeMap kind (apply sorted-map kvs) leftfn rightfn)))
 
 
 (defrecord B+Tree [b slots])
