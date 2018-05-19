82a26258c138ad6375a3f1812373087a529d579d
Oliver Wegner <void1976@gmail.com>
2018-05-19 22:50:46 +0200

    working on b+tree

--- sample/src/sample/btree.clj
+++ sample/src/sample/btree.clj
@@ -9,16 +9,24 @@
   (search [this k]))
 
 
-(defrecord B+Tree [b slots])
+(defrecord B+Tree [b key-fn
+                   slots root? leaf?])
 
 (extend-type B+Tree
 
   TreeModify
 
-  (add [{:keys [slots] :as this} v]
-    )
+  (add [{:keys [key-fn slots] :as this} v]
+    (assoc slots (key-fn v) v))
 
   TreeSearch
 
   (search [this k]
     ))
+
+
+(defn b+tree [b key-fn & {:keys [root? leaf?]
+                          :or {root? true
+                               leaf? true}}]
+  (->B+Tree b key-fn
+            (rm/range-map :find-ceiling) root? leaf?))
