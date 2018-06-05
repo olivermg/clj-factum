49e83c1db4e959168ed86081c11c30bcfea23343
Oliver Wegner <void1976@gmail.com>
2018-06-05 17:28:19 +0200

    working on b+tree

--- sample/src/sample/btree.clj
+++ sample/src/sample/btree.clj
@@ -85,34 +85,136 @@
   (->B+TreeChildren (rm/range-map :find-ceiling) nil))
 
 
+(defn- internal-node [ks vs]
+  {:pre [(= (count vs)
+            (inc (count ks)))]}
+  {:ks ks
+   :vs vs})
 
-(defrecord B+Tree [b size children leaf?]
+(defn- inode-lookup [{:keys [ks vs] :as n} k]
+  ;;; TODO: implement binary search
+  (loop [[k* & ks*] ks
+         [v* & vs*] vs]
+    (cond
+      (nil? k*)             v*
+      (<= (compare k k*) 0) v*
+      true                  (recur ks* vs*))))
+
+(defn- inode-split [{:keys [ks vs] :as n}]
+  (let [partition-size  (-> ks count (/ 2) Math/ceil int)
+        [ks1 ks2]       (partition-all partition-size ks)
+        [ks1 nk]        [(butlast ks1) (last ks1)]
+        [vs1 vs2a vs2b] (partition-all partition-size vs)
+        vs2             (concat vs2a vs2b)]
+    [(internal-node ks1 vs1)
+     nk
+     (internal-node ks2 vs2)]))
+
+(defn- leaf-node [ks vs]
+  {:pre [(= (count vs)
+            (count ks))]}
+  (zipmap ks vs))
+
+(defn- lnode-lookup [n k]
+  (get n k))
+
+(defn- lnode-split [n]
+  (let [partition-size (-> n count (/ 2) Math/ceil int)
+        [ks1 ks2] (partition-all partition-size (keys n))
+        vs1 (apply dissoc n ks2)
+        vs2 (apply dissoc n ks1)]
+    [(leaf-node ks1 vs1)
+     (last ks1)
+     (leaf-node ks2 vs2)]))
+
+(defn- lnode-insert [n k v]
+  )
+
+
+
+(defrecord B+TreeNode [b size children leaf?]
 
   TreeModify
 
   (add [this k v]
-    (letfn [(new-leafnode []
-              (->B+Tree b 0 (rm/range-map :find-ceiling nil) true))
-            (new-interiornode []
-              (->B+Tree b 0 (rm/range-map :find-ceiling (->B+Tree b 0 (rm/range-map :find-ceiling nil))) false))
-            (balance [{:keys [children] :as this}]
-              (if (< (:size children) b)
-                this
-                ))]
+    (letfn [(split [children size]
+              (println "SPLIT1" (keys children) (type children))
+              (let [partition-size (-> size (/ 2) Math/ceil int)
+                    [ks1 ks2] (partition-all partition-size (keys children))
+                    cs1 (apply dissoc children ks2)  ;; cannot use select-keys here, as that removes sorted-map properties
+                    cs2 (apply dissoc children ks1)]
+                (println "SPLIT2" ks1 ks2 (type cs1) (type cs2))
+                (let [res [{:c cs1
+                            :s partition-size
+                            :k (last ks1)}
+                           {:c cs2
+                            :s (- partition-size (rem size 2))
+                            :k (last ks2)}]]
+                  (println "SPLIT3" res)
+                  res)))
 
-      (if leaf?
-        (if (< size b)
-          [(->B+Tree b (inc size) (assoc children k v) leaf?)]
-          (let [children (assoc children k v)
-                size (inc size)
-                [ks1 ks2] (partition-all (-> size (/ 2) Math/ceil int) (keys children))
-                [cs1 cs2] [(select-keys children ks1) (select-keys children ks2)]]
-            [(->B+Tree b (count cs1) cs1 leaf?)
-             (->B+Tree b (count cs2) cs2 leaf?)]))
+            (ins [children size k v]
+              (println "INS" size k v children)
+              (let [children (assoc children k v)
+                    size (inc size)]
+                (if (< size b)
+                  [{:c children :s size :k k}]
+                  (split children size))))
 
-        (let [child (get children k)]
-          (add child k v)
-          #_(->B+Tree b (inc size) (assoc children k v) leaf?)))))
+            (add* [{:keys [b children size leaf?] :as this} k v root? i]
+              #_(println "ADD*" k v root? leaf?)
+              (if (<= i 5)
+                (cond
+                  leaf? (let [_ (println "LEAF1" k v)
+                              [m1 m2] (ins children size k v)]
+                          (println "LEAF2" m1 m2)
+                          [{:n (->B+TreeNode b (:s m1) (:c m1) true)
+                            :k (:k m1)}
+                           (when-not (nil? m2)
+                             {:n (->B+TreeNode b (:s m2) (:c m2) true)
+                              :k (:k m2)})])
+                  #_root? #_(let [_ (println "ROOT1" k v)
+                              child (get children k)
+                              [b1 sk b2] (add* child k v false (inc i))]
+                          (println "ROOT2" b1 sk b2)
+                          [(if (nil? b2)
+                             b1
+                             (->B+TreeNode b 1 (rm/range-map :find-ceiling b2 sk b1) false))])
+                  true (let [_ (println "TRUE1" k v)
+                             child (get children k)
+                             orig-k (-> child meta :sample.rangemap/key)
+                             [b1 b2] (add* child k v false (inc i))]
+                         (println "TRUE2" b1 b2 orig-k)
+                         (if (nil? b2)
+                           [{:n (->B+TreeNode b size (assoc children orig-k (:n b1)) leaf?)
+                             :k orig-k}]
+                           (let [children   (-> (dissoc children k)
+                                                (assoc (:k b1) (:n b1)))
+                                 [m1 m2] (ins children size (:k b2) (:n b2))]
+                             (println "TRUE3" m1 m2)
+                             [{:n (->B+TreeNode b (:s m1) (:c m1) false)
+                               :k (:k m1)}
+                              (when-not (nil? m2)
+                                {:n (->B+TreeNode b (:s m2) (:c m2) false)
+                                 :k (:k m2)})]))))
+                (do (println "ABORT")
+                    [this])))]
+
+      (println "=== ADD* ===" k v)
+      (let [[b1 b2] (add* this k v true 0)]
+        (println "ADD*2" b1 b2)
+        (let [res (if (nil? b2)
+                    (:n b1)
+                    (->B+TreeNode b 1
+                                  (-> (rm/range-map :find-ceiling)
+                                      (assoc (:k b1) (:n b1) :sample.rangemap/fallback (:n b2)))
+                                  false))]
+          (println "ADD*3" res)
+          res))
+
+      (split (-> (rm/range-map :find-ceiling)
+                 (assoc 1 11 2 22 3 33 4 44 :sample.rangemap/fallback 55))
+             5)))
 
   TreeSearch
 
@@ -124,4 +226,4 @@
 
 
 (defn b+tree [b]
-  (->B+Tree b 0 (rm/range-map :find-ceiling nil) true))
+  (->B+TreeNode b 0 (sorted-map) true))
--- sample/src/sample/rangemap.clj
+++ sample/src/sample/rangemap.clj
@@ -8,13 +8,19 @@
   clojure.lang.IPersistentMap
 
   (assoc [this k v]
-    (RangeMap. kind (.assoc m k v) leftfn rightfn fallback-val))
+    (if-not (= k ::fallback)
+      (RangeMap. kind (.assoc m k v) leftfn rightfn fallback-val)
+      (RangeMap. kind m leftfn rightfn v)))
 
   (assocEx [this k v]
-    (RangeMap. kind (.assocEx m k v) leftfn rightfn fallback-val))
+    (if-not (= k ::fallback)
+      (RangeMap. kind (.assocEx m k v) leftfn rightfn fallback-val)
+      (RangeMap. kind m leftfn rightfn v)))
 
   (without [this k]
-    (RangeMap. kind (.without m k) leftfn rightfn fallback-val))
+    (if-not (= k ::fallback)
+      (RangeMap. kind (.without m k) leftfn rightfn fallback-val)
+      (RangeMap. kind m leftfn rightfn nil)))
 
 
   clojure.lang.ILookup
@@ -23,23 +29,28 @@
     (.valAt this k nil))
 
   (valAt [this k not-found]
-    (let [idx (loop [ks     (keys m)
-                     n      (count ks)
-                     lastki nil]
-                #_(println "LOOP1" lastki ks)
-                (if (not-empty ks)
-                  (let [i   (int (/ n 2))
-                        ki  (get (vec ks) i)
-                        cmp (compare ki k)]
-                    #_(println "LOOP2" n i ki cmp)
-                    (cond
-                      (> cmp 0) (recur (take i ks)       i         (leftfn lastki ki))   ;; traverse left
-                      (< cmp 0) (recur (drop (inc i) ks) (- n i 1) (rightfn lastki ki))  ;; traverse right
-                      (= cmp 0) ki))
-                  lastki))]
-      (if-not (nil? idx)
-        (get m idx)
-        (or not-found fallback-val))))
+    (if-not (= k ::fallback)
+      (let [idx (loop [ks     (keys m)
+                       n      (count ks)
+                       lastki nil]
+                  #_(println "LOOP1" lastki ks)
+                  (if (not-empty ks)
+                    (let [i   (int (/ n 2))
+                          ki  (get (vec ks) i)
+                          cmp (compare ki k)]
+                      #_(println "LOOP2" n i ki cmp)
+                      (cond
+                        (> cmp 0) (recur (take i ks)       i         (leftfn lastki ki))   ;; traverse left
+                        (< cmp 0) (recur (drop (inc i) ks) (- n i 1) (rightfn lastki ki))  ;; traverse right
+                        (= cmp 0) ki))
+                    lastki))]
+        (if-not (nil? idx)
+          (with-meta (get m idx)
+            {::key idx})
+          (or not-found
+              (with-meta fallback-val
+                {::key ::fallback}))))
+      fallback-val))
 
 
   clojure.lang.IFn
@@ -51,13 +62,17 @@
   clojure.lang.Seqable
 
   (seq [this]
-    (.seq m))
+    (concat (.seq m)
+            (when-not (nil? fallback-val)
+              [(clojure.lang.MapEntry. ::fallback fallback-val)])))
 
 
   clojure.lang.Associative
 
   (containsKey [this k]
-    (.containsKey m k))
+    (if-not (= k ::fallback)
+      (.containsKey m k)
+      true))
 
   (entryAt [this k]
     (.entryAt m k))
@@ -69,7 +84,7 @@
     (.equiv m o))
 
   (empty [this]
-    (RangeMap. kind (.empty m) leftfn rightfn fallback-val))
+    (RangeMap. kind (.empty m) leftfn rightfn nil))
 
   (cons [this o]
     (RangeMap. kind (.cons m o) leftfn rightfn fallback-val))
@@ -87,7 +102,14 @@
   Object
 
   (toString [this]
-    (.toString m))
+    (let [s (->> (pr-str m)
+                 rest
+                 butlast
+                 (apply str))]
+      (str "{" s
+           (when-not (nil? fallback-val)
+             (str " " ::fallback " " (pr-str fallback-val)))
+           "}")))
 
 
   java.util.Map
@@ -105,12 +127,17 @@
     (.hasheq m)))
 
 
-(defn range-map [kind fallback-val & kvs]
+(defmethod print-method RangeMap [o writer]
+  (.write writer (.toString o)))
+
+
+(defn range-map [kind & kvs]
   {:pre [(or (= kind :find-ceiling)
              (= kind :find-floor))]}
+  (println "RANGE-MAP" kind kvs)
   (let [[leftfn rightfn] (case kind
                            :find-floor   [(fn [lastki ki] lastki)
                                           (fn [lastki ki] ki)]
                            :find-ceiling [(fn [lastki ki] ki)
                                           (fn [lastki ki] lastki)])]
-    (->RangeMap kind (apply sorted-map kvs) leftfn rightfn fallback-val)))
+    (->RangeMap kind (apply sorted-map kvs) leftfn rightfn nil)))
